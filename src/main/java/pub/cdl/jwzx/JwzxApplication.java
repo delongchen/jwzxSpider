38aa61e06c68fb0369c19e52c9f99343db457fde
delongchen <fg456@vip.qq.com>
2020-05-10 22:08:21 +0800

    init

---
+++ src/main/java/pub/cdl/jwzx/JwzxApplication.java
@@ -0,0 +1,13 @@
+package pub.cdl.jwzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwzxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwzxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pub/cdl/jwzx/JwzxApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.cdl.jwzx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwzxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
